09f0fe8d128b6a6bcca6d10253258d7a55a99a3d
hallgato <hallgato@LABOR114-17>
2023-11-16 17:28:59 +0100

    added JsonIgnore attributes to model Grade

--- YT7G72_HFT_2023241.Models/Models/Grade.cs
+++ YT7G72_HFT_2023241.Models/Models/Grade.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace YT7G72_HFT_2023241.Models
 {
@@ -19,8 +20,11 @@ namespace YT7G72_HFT_2023241.Models
         public int SubjectId { get; set; }
         [Required]
         public int TeacherId { get; set; }
+        [JsonIgnore]
         public virtual Student Student { get; set; }
+        [JsonIgnore]
         public virtual Subject Subject { get; set; }
+        [JsonIgnore]
         public virtual Teacher Teacher { get; set; }
         [Required]
         [Format("Integer between 1 and 5")]
