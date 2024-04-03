a7a0e2902ea5e056367c095bbcb49cc9a55c89b4
therealuhlarzoltan <therealuhlarzoltan@gmail.com>
2024-04-03 11:56:14 +0200

    update

---
+++ YT7G72_HFT_2023241.WpfClient/ViewModels/SubjectStatisticsWindowViewModel.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YT7G72_HFT_2023241.WpfClient.Logic;
+using YT7G72_HFT_2023241.Models;
+
+namespace YT7G72_HFT_2023241.WpfClient.ViewModels
+{
+    public class SubjectStatisticsWindowViewModel : ObservableRecipient, IDisposable
+    {
+        private RestService restService;
+        private int subjectId;
+        private SubjectStatistics subjectStatistics;
+
+        public SubjectStatistics SubjectStatistics
+        {
+            get { return subjectStatistics; }
+            set
+            {
+                SetProperty(ref subjectStatistics, value);
+            }
+        }
+
+        public SubjectStatisticsWindowViewModel(RestService restService, int subjectId) 
+        {
+            this.restService = restService;
+            this.subjectId = subjectId;
+        }
+
+        private void RegisterMessengers()
+        {
+
+        }
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ YT7G72_HFT_2023241.WpfClient/Windows/SubjectStatisticsWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using YT7G72_HFT_2023241.WpfClient.ViewModels;
+
+namespace YT7G72_HFT_2023241.WpfClient.Windows
+{
+    /// <summary>
+    /// Interaction logic for SubjectStatisticsWindow.xaml
+    /// </summary>
+    public partial class SubjectStatisticsWindow : Window
+    {
+        public SubjectStatisticsWindow()
+        {
+            InitializeComponent();
+        }
+
+        public SubjectStatisticsWindow(SubjectCreateWindowViewModel vm) : this()
+        {
+            this.DataContext = vm;
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            (this.DataContext as SubjectStatisticsWindowViewModel)?.Dispose();
+            base.OnClosed(e);
+        }
+    }
+}
